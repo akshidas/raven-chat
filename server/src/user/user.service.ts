1f028fc6efa6b842e07c194c2a476b68e55cbb31
Akshay <akshaydas.dev@gmail.com>
2022-12-19 07:53:10 +0530

    added conflict exception

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,5 +1,11 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { sign } from 'jsonwebtoken';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -8,13 +14,27 @@ import { User, UserDocument } from './entities/user.entity';
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async genToken(payload: any) {
+    return await sign(payload, 'shhhhh');
+  }
+
   async create(createUserDto: CreateUserDto) {
     try {
       const user = new this.userModel(createUserDto);
-      await user.save();
-      return { message: `User Created`, statusCode: HttpStatus.OK };
+      const userExist = await this.userModel.exists({
+        username: createUserDto.username,
+      });
+      if (userExist) {
+        return new ConflictException();
+      }
+      const savedUser = await user.save();
+
+      return {
+        accessToken: await this.genToken({ id: savedUser.id }),
+      };
     } catch (err) {
-      throw new HttpException(`User already exist`, HttpStatus.CONFLICT);
+      throw new InternalServerErrorException();
     }
   }
 
@@ -23,8 +43,12 @@ export class UserService {
     return usersList;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    try {
+      const user = await this.userModel.findById(id, '-password');
+
+      return user;
+    } catch (err) {}
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
